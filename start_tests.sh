d9eaa2d4e4610f4e060d7d5e73eebd6979d3d6f9
W Millon <wjmillon@gmail.com>
2020-04-10 15:57:21 -0500

    file correction

--- start_tests.sh
+++ start_tests.sh
@@ -2,20 +2,21 @@
 
 set -e
 
-mix do local.hex --force, local.rebar --force
-
 pg_ctlcluster 11 main start
 
 sleep 8
 
 su - postgres && psql -U postgres -c "CREATE USER $ELIXIR_DB_USER WITH CREATEDB LOGIN ENCRYPTED PASSWORD '$DB_PW'"
 
-mix do clean, compile --force --warning-as-errors
-
-mix format --check-formatted
-
-mix credo --strict
-
-MIX_ENV=test mix do ecto.create, ecto.migrate, test --trace
+mix do \
+  local.hex --force, \
+  local.rebar --force, \
+  clean, \
+  compile --force --warning-as-errors, \
+  format --check-formatted, \
+  credo --strict, \
+  MIX_ENV=test ecto.create, \
+  MIX_ENV=test ecto.migrate, \
+  test --trace
 
 # mix dialyzer
