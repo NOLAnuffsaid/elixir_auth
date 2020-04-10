1e49e6d4cc74d6b7da02469f40d395fae8bd191b
W Millon <wjmillon@gmail.com>
2020-04-10 16:07:54 -0500

    file correction

--- start_tests.sh
+++ start_tests.sh
@@ -4,15 +4,15 @@ set -e
 
 pg_ctlcluster 11 main start
 
-mix do \
+MIX_ENV=test mix do \
   local.hex --force, \
   local.rebar --force, \
   clean, \
   compile --force --warning-as-errors, \
   format --check-formatted, \
   credo --strict, \
-  MIX_ENV=test ecto.create, \
-  MIX_ENV=test ecto.migrate, \
+  ecto.create, \
+  ecto.migrate, \
   test --trace
 
 # mix dialyzer
