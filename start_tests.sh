d062b4503400874b1034e3d83b388fc6b9f234ad
W Millon <wjmillon@gmail.com>
2020-04-10 11:49:10 -0500

    file correction

--- start_tests.sh
+++ start_tests.sh
@@ -2,12 +2,6 @@
 
 set -e
 
-MIX_ENV=test
-
-mix do local.hex --force, local.rebar --force
-
-mix do deps.get, deps.get  #, dialyzer --plt
-
 mix do clean, compile --force --warning-as-errors
 
 mix format --check-formatted
