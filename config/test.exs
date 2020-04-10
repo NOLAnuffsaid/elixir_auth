5b1b276b941de602841dfb8baea606dc30b181bd
W Millon <wjmillon@gmail.com>
2020-04-10 07:15:08 -0500

    file correction

--- config/test.exs
+++ config/test.exs
@@ -21,6 +21,7 @@ config :pbkdf2_elixir, :rounds, 1
 
 config :junit_formatter,
   report_file: "report_file_test.xml",
+  report_dir: "./",
   print_report_file: true,
   prepend_project_name?: true,
   include_filename?: true
