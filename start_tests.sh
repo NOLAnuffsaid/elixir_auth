581578bc50bbb2290cb491e0c5c6e592c18be6ea
W Millon <wjmillon@gmail.com>
2020-04-10 16:02:21 -0500

    file correction

--- start_tests.sh
+++ start_tests.sh
@@ -4,10 +4,6 @@ set -e
 
 pg_ctlcluster 11 main start
 
-sleep 8
-
-su - postgres && psql -U postgres -c "CREATE USER $ELIXIR_DB_USER WITH CREATEDB LOGIN ENCRYPTED PASSWORD '$DB_PW'"
-
 mix do \
   local.hex --force, \
   local.rebar --force, \
